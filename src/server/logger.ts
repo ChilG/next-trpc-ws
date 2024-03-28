a173260a5175349f5152b17611d41c72885ad852
chilgoe <zonda037@gmail.com>
2024-03-28 20:26:40 +0700

    add Dockerfile

--- src/server/logger.ts
+++ src/server/logger.ts
@@ -1,9 +1,7 @@
-import chalk from 'chalk';
-
 export const info = (text: string) => {
-  console.log(chalk`{cyan [next-custom-server]} ${text}`);
+  console.log('\x1b[34m', `[next-custom-server] ${text}`);
 };
 
 export const error = (message: string) => {
-  console.log(chalk`{cyan [next-custom-server]} {red ${message}}`);
+  console.log('\x1b[31m', `[next-custom-server] ${message}`);
 };
